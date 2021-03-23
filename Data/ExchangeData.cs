e0fbf4ac895b24b7a41b8159f5d478dcfd302239
WaitingToCompile <git@edken.co.uk>
2021-03-23 17:32:45 +0000

    Made ExchangeData thread safe

--- Data/ExchangeData.cs
+++ Data/ExchangeData.cs
@@ -1,10 +1,14 @@
 ﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace FIOSharp.Data
 {
 	public class ExchangeData
 	{
+		private object guard = new object();
+
 		[JsonProperty("ExchangeName")]
 		[JsonRequired]
 		public readonly string Name;
@@ -26,18 +30,23 @@ namespace FIOSharp.Data
 
 		[JsonIgnore]
 		public IReadOnlyDictionary<Material, ExchangeEntry> ExchangeEntries => _exchangeEntries;
+		//we're using a ConcurrentDictionary since it might be edited by asynchronous methods
 		[JsonIgnore]
-		private Dictionary<Material, ExchangeEntry> _exchangeEntries = new Dictionary<Material, ExchangeEntry>();
+		private ConcurrentDictionary<Material, ExchangeEntry> _exchangeEntries = new ConcurrentDictionary<Material, ExchangeEntry>();
 
 		public string GetComexMaterialCode(string materialTicker)
 		{
 			return materialTicker + "." + Ticker;
 		}
 
-
+		/// <summary>
+		/// update our exchange data with the entry for a given commodity in a thread safe manner
+		/// </summary>
+		/// <param name="entry">the entry to be applied</param>
 		internal void UpdateWithCommodityEntry(ExchangeEntry entry)
 		{
-			_exchangeEntries[entry.Material] = entry;
+			if (entry.Exchange != this) throw new ArgumentException(nameof(entry), "Entry passed was for a different exchange");
+			_exchangeEntries.AddOrUpdate(entry.Material, entry, (key, value) => entry);
 		}
 	}
 }
