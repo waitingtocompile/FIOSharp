b90ff897aa0e4cb5390746807c3443999f97e3e3
WaitingToCompile <git@edken.co.uk>
2021-03-23 17:57:38 +0000

    Updated data source interfaces with Async method signiatures

--- IFixedDataSource.cs
+++ IFixedDataSource.cs
@@ -1,5 +1,6 @@
 ﻿using FIOSharp.Data;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FIOSharp
 {
@@ -8,6 +9,9 @@ namespace FIOSharp
 	/// </summary>
 	public interface IFixedDataSource
 	{
+		/// <summary>
+		/// Get a list of all materials. This includes special "hidden" materials like CMK
+		/// </summary>
 		public List<Material> GetMaterials();
 		/// <summary>
 		/// Try to get a list of all buildings. This can result in cascading data access.
@@ -25,5 +29,26 @@ namespace FIOSharp
 		/// To prevent cascading, pass a list of all materials
 		/// </summary>
 		public List<WorkforceRequirement> GetWorkforceRequirements(List<Material> allMaterials = null);
+
+		/// <summary>
+		/// Get a list of all materials. This includes special "hidden" materials like CMK
+		/// </summary>
+		public Task<List<Material>> GetMaterialsAsync();
+		/// <summary>
+		/// Try to get a list of all buildings. This can result in cascading data access.
+		/// To prevent cascading, pass a list of all materials.
+		/// </summary>
+		public Task<List<Building>> GetBuildingsAsync(List<Material> allMaterials = null);
+		/// <summary>
+		/// Try to get a list of all recipes. This can result in cascading data access.
+		/// To prevent cascading, pass a list of all materials and a list of all buildings
+		/// </summary>
+		public Task<List<Recipe>> GetRecipesAsync(List<Material> allMaterials = null, List<Building> buildings = null);		
+
+		/// <summary>
+		/// Try to get the consumable requirements for each population type. This can result in cascading data access.
+		/// To prevent cascading, pass a list of all materials
+		/// </summary>
+		public Task<List<WorkforceRequirement>> GetWorkforceRequirementsAsync(List<Material> allMaterials = null);
 	}
 }
--- IVariableDataSource.cs
+++ IVariableDataSource.cs
@@ -1,5 +1,6 @@
 ﻿using FIOSharp.Data;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FIOSharp
 {
@@ -35,6 +36,31 @@ namespace FIOSharp
 		/// <param name="applyToExchange">if the found entry should be used to update our exchange</param>
 		public ExchangeEntry GetEntryForExchange(ExchangeData exchange, Material material, bool applyToExchange = true);
 
+		/// <summary>
+		/// Get a List of all Commodity exchanges
+		/// </summary>
+		public Task<List<ExchangeData>> GetExchangesAsync();
 
+		/// <summary>
+		/// Get all commodity entries for a given commodity exchange. This can result in cascading data access. To prevent cascading, pass a list of all materials
+		/// If you need data for multiple exchanges it will almost always be quicker to use GetEntriesForExchanges
+		/// </summary>
+		/// <param name="exchange">the exchange to look up data for</param>
+		/// <param name="applyToExchange">if the found entries should be used to update our exchange</param>
+		public Task<List<ExchangeEntry>> GetEntriesForExchangeAsync(ExchangeData exchange, List<Material> allMaterials = null, bool applyToExchange = true);
+		/// <summary>
+		/// Get all commodities for several commodity exchanges. This can result in cascading data access. To prevent cascading, pass a list of all materials
+		/// </summary>
+		/// <param name="exchanges">the exchanges to look up data for</param>
+		/// <param name="applyToExchanges">if the found entries should be used to update our exchanges</param>
+		public Task<List<ExchangeEntry>> GetEntriesForExchangesAsync(List<ExchangeData> exchanges, List<Material> allMaterials = null, bool applyToExchanges = true);
+		/// <summary>
+		/// Look up the commodity entry of a given material for a given exchange.
+		/// If you need to look up many materials, you should instead be using GetEntriesForExchange where possible
+		/// </summary>
+		/// <param name="exchange">the exchange to look up data for</param>
+		/// <param name="material">the material we are getting data on</param>
+		/// <param name="applyToExchange">if the found entry should be used to update our exchange</param>
+		public Task<ExchangeEntry> GetEntryForExchangeAsync(ExchangeData exchange, Material material, bool applyToExchange = true);
 	}
 }
