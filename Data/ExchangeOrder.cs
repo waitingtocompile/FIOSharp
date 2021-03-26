e306f481f413e665e5f11cce049af482c6bc3103
WaitingToCompile <git@edken.co.uk>
2021-03-26 16:20:09 +0000

    Remove unnused imports and dead files

--- Data/ExchangeOrder.cs
+++ Data/ExchangeOrder.cs
@@ -1,7 +1,4 @@
-﻿
-
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
 
 namespace FIOSharp.Data
 {
--- FnarOracleDataSource.cs
+++ FnarOracleDataSource.cs
@@ -1,13 +1,11 @@
 ﻿using FIOSharp.Data;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Schema;
 using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FIOSharp
