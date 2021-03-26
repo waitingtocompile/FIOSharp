e8d14747dbc6eab27ab32fe5d3f43a603d1dbd8d
WaitingToCompile <git@edken.co.uk>
2021-03-26 15:38:34 +0000

    Fixed spelling error (and resulting parse errors)

--- Data/PopulationType.cs
+++ Data/PopulationType.cs
@@ -7,14 +7,14 @@ namespace FIOSharp.Data
 	{
 		public static readonly PopulationType PIONEER = new PopulationType("Pioneer");
 		public static readonly PopulationType SETTLER = new PopulationType("Settler");
-		public static readonly PopulationType TECHNITIAN = new PopulationType("Technitian");
+		public static readonly PopulationType TECHNICIAN = new PopulationType("Technician");
 		public static readonly PopulationType ENGINEER = new PopulationType("Engineer");
 		public static readonly PopulationType SCIENTIST = new PopulationType("Scientist");
 		public static PopulationType[] ALL { get
 			{
 				if(_all == null)
 				{
-					_all = new PopulationType[] { PIONEER, SETTLER, TECHNITIAN, ENGINEER, SCIENTIST };
+					_all = new PopulationType[] { PIONEER, SETTLER, TECHNICIAN, ENGINEER, SCIENTIST };
 				}
 				return _all;
 			} }
